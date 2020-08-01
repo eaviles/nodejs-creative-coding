cc82d259ee6560c5769dad1960cac3b9cceba5e0
Edgardo Avilés <eaviles@gmail.com>
2020-08-01 05:21:20 -0400

    Add text: drawing text

--- text/app.js
+++ text/app.js
@@ -1,4 +1,5 @@
 const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math');
 const palettes = require('nice-color-palettes');
 const random = require('canvas-sketch-util/random');
 
@@ -14,13 +15,16 @@ const sketch = () => {
   const palette = random.shuffle(random.pick(palettes));
 
   const step = 10;
-  for (let x = step; x < 800; x += step) {
-    for (let y = step; y < 800; y += step) {
-      if (Math.abs(random.gaussian()) > 1.0) {
+  for (let x = 0; x < 800; x += step) {
+    for (let y = 0; y < 800; y += step) {
+      if (Math.random() > 0.5) {
+        const nx = math.mapRange(x, 0, 800, 0, 1);
+        const ny = math.mapRange(y, 0, 800, 0, 1);
         points.push({
           position: [x, y],
-          radius: Math.abs(random.noise2D(x, y, 0.0025, step / 2)),
-          color: random.rangeFloor(1, palette.length - 1)
+          radius: Math.abs(random.noise2D(nx, ny, 0.1, 30)) + 5,
+          color: random.rangeFloor(1, palette.length - 1),
+          rotation: random.noise2D(nx, ny)
         });
       }
     }
@@ -30,12 +34,14 @@ const sketch = () => {
     context.fillStyle = palette[0];
     context.fillRect(0, 0, width, height);
 
-    points.forEach(({ color, position, radius }) => {
+    points.forEach(({ color, position, radius, rotation }) => {
       const [x, y] = position;
       context.fillStyle = palette[color];
-      context.beginPath();
-      context.arc(x, y, radius, 0, Math.PI * 2);
-      context.fill();
+      context.save();
+      context.font = `${Math.round(radius)}px Menlo`;
+      context.rotate(rotation);
+      context.fillText('#sfpc', x, y);
+      context.restore();
     });
   };
 };
